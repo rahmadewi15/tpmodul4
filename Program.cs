127ac205c1b7c6e0864a6710d73814ce808b7e72
Irkhamna Rahma Dewi <rahmadewi@student.telkomuniversity.ac.id>
2022-03-18 15:50:21 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302201132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nama User: ");
+
+            String a = Console.ReadLine();
+            HaloGeneric.SapaUser<String>(a);
+
+            Console.WriteLine("NIM User: ");
+            String NIM = Console.ReadLine();
+            DataGeneric<String> c = new DataGeneric<string>(NIM);
+            c.PrintData();
+        }
+    }
+    class HaloGeneric
+    {
+        public static void SapaUser<R>(R nama)
+        {
+            Console.WriteLine("Halo User " + nama);
+
+        }
+    }
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("NIM: " + data);
+
+        }
+    }
+}
